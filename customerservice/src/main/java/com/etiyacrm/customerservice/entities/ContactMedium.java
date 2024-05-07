9919a3d4ddece920ee7145f7ef4899951f938ef2
ysertac <ysertac96@gmail.com>
2024-05-07 09:15:32 -0700

    add ContactMedium entity

---
+++ customerservice/src/main/java/com/etiyacrm/customerservice/entities/ContactMedium.java
@@ -0,0 +1,31 @@
+package com.etiyacrm.customerservice.entities;
+
+import com.etiyacrm.customerservice.core.entities.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "contact_medium")
+public class ContactMedium extends BaseEntity {
+    @Id
+    @GeneratedValue
+    @Column(name = "id")
+    private long id;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "home_phone")
+    private String homePhone;
+
+    @Column(name = "mobile_phone")
+    private String mobilePhone;
+
+    @Column(name = "fax")
+    private String fax;
+}
