7cb2237163b2a8627b95b6ef19a0048e1a1bb5ea
ysertac <ysertac96@gmail.com>
2024-05-13 07:08:23 -0700

    add Validation Rules and Messages for CreateContactMediumRequest and CreateIndividualCustomerRequest

--- customerservice/src/main/java/com/etiyacrm/customerservice/services/dtos/requests/contactMediumRequests/CreateContactMediumRequest.java
+++ customerservice/src/main/java/com/etiyacrm/customerservice/services/dtos/requests/contactMediumRequests/CreateContactMediumRequest.java
@@ -1,10 +1,6 @@
 package com.etiyacrm.customerservice.services.dtos.requests.contactMediumRequests;
 
-import com.etiyacrm.customerservice.entities.Customer;
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Column;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.OneToOne;
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,9 +9,18 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class CreateContactMediumRequest {
+    @NotNull
+    @Pattern(regexp = "\\b[A-Za-z0-9._%+-]+@(?:[A-Za-z0-9-]+\\.)+[A-Z|a-z]{2,}\\b", message = "E-mail must be a valid e-mail address")
     private String email;
+
+    @Pattern(regexp = "^\\d{10}$", message = "Home Phone must have 10 characters and contain only numbers.")
     private String homePhone;
+
+    @NotNull
+    @Pattern(regexp = "^5\\d{9}$", message = "Phone number must be written in (5xx)-xxx-xx-xx format")
     private String mobilePhone;
+
+    @Pattern(regexp = "^\\d{10}$", message = "Fax must have 10 characters and contain only numbers.")
     private String fax;
     private String customerId;
 }
