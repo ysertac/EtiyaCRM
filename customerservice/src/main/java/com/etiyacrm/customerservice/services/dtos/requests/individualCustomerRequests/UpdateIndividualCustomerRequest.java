56998987b61f69f8c735e8aa6afa46dabfde8064
ysertac <ysertac96@gmail.com>
2024-05-13 07:51:07 -0700

    add Validation Rules and Messages for UpdateContactMediumRequest and UpdateIndividualCustomerRequest

--- customerservice/src/main/java/com/etiyacrm/customerservice/services/dtos/requests/individualCustomerRequests/UpdateIndividualCustomerRequest.java
+++ customerservice/src/main/java/com/etiyacrm/customerservice/services/dtos/requests/individualCustomerRequests/UpdateIndividualCustomerRequest.java
@@ -1,5 +1,9 @@
 package com.etiyacrm.customerservice.services.dtos.requests.individualCustomerRequests;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,12 +14,36 @@ import java.time.LocalDate;
 @AllArgsConstructor
 @NoArgsConstructor
 public class UpdateIndividualCustomerRequest {
+    @NotNull
+    @NotBlank(message = "First name can not be empty")
+    @Size(min = 3, max = 30, message = "First name may have minimum 3 and maximum 30 characters")
     private String firstName;
+
+    @Size(max = 30, message = "Middle name may have maximum 30 characters")
     private String middleName;
+
+    @NotNull
+    @NotBlank(message = "Last name can not be empty")
+    @Size(min = 3, max = 30, message = "Last name may have minimum 3 and maximum 30 characters")
     private String lastName;
+
+    @NotNull
+    @NotBlank(message = "Gender can not be empty")
     private String gender;
+
+    @NotNull
+    @NotBlank(message = "Mother name can not be empty")
+    @Size(min = 3, max = 30, message = "Mother name may have minimum 3 and maximum 30 characters")
     private String motherName;
+
+    @NotNull
+    @NotBlank(message = "Father name can not be empty")
+    @Size(min = 3, max = 30, message = "Father name may have minimum 3 and maximum 30 characters")
     private String fatherName;
+
+    @NotNull
+    @Pattern(regexp = "^[1-9]{1}[0-9]{9}[02468]{1}$", message = "Invalid National ID number ")
     private String nationalityId;
+
     private LocalDate birthDate;
 }
