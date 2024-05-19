c008be231288b2e0d18c7631187aed7d61963bd2
Ertan SİDAR <ertan.sidar@etiya.com>
2024-05-19 13:54:23 +0300

    complete catalog service

--- catalogservice/src/main/java/com/etiya/catalogservice/CatalogserviceApplication.java
+++ catalogservice/src/main/java/com/etiya/catalogservice/CatalogserviceApplication.java
@@ -2,6 +2,7 @@ package com.etiya.catalogservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CatalogserviceApplication {
--- catalogservice/src/main/java/com/etiya/catalogservice/services/mappers/CharacteristicValueMapper.java
+++ catalogservice/src/main/java/com/etiya/catalogservice/services/mappers/CharacteristicValueMapper.java
@@ -5,22 +5,29 @@ import com.etiya.catalogservice.services.dtos.requests.characteristicValueReques
 import com.etiya.catalogservice.services.dtos.requests.characteristicValueRequests.UpdateCharacteristicValueRequest;
 import com.etiya.catalogservice.services.dtos.responses.characteristicValueResponses.*;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
 @Mapper
 public interface CharacteristicValueMapper {
     CharacteristicValueMapper INSTANCE = Mappers.getMapper(CharacteristicValueMapper.class);
-
+    
+    @Mapping(source = "characteristicValue.characteristic.id", target = "characteristicId")
     GetAllCharacteristicValueResponse getAllCharacteristicValueResponseFromCharacteristicValue(CharacteristicValue characteristicValue);
 
+    @Mapping(source = "characteristic.id", target = "characteristicId")
     GetCharacteristicValueResponse getCharacteristicValueResponseFromCharacteristicValue(CharacteristicValue characteristicValue);
 
+    @Mapping(source = "characteristicId", target = "characteristic.id")
     CharacteristicValue getCharacteristicValueFromCreateCharacteristicValueRequest(CreateCharacteristicValueRequest createCharacteristicValueRequest);
 
+    @Mapping(source = "characteristic.id", target = "characteristicId")
     CreatedCharacteristicValueResponse getCreatedCharacteristicValueResponseFromCharacteristicValue(CharacteristicValue characteristicValue);
 
+    @Mapping(source = "characteristicId", target = "characteristic.id")
     CharacteristicValue getCharacteristicValueFromUpdateCharacteristicValueRequest(UpdateCharacteristicValueRequest updateCharacteristicValueRequest);
 
+    @Mapping(source = "characteristic.id", target = "characteristicId")
     UpdatedCharacteristicValueResponse getUpdatedCharacteristicValueResponseFromCharacteristicValue(CharacteristicValue characteristicValue);
 
     DeletedCharacteristicValueResponse getDeletedCharacteristicValueResponseFromCharacteristicValue(CharacteristicValue characteristicValue);
