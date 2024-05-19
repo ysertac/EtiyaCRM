25bf68a58315abf4dfd3c0dfe0427441fa0e890f
enesokur <enes.okur@etiya.com>
2024-05-19 15:25:43 +0300

    add responses

--- catalogservice/src/main/java/com/etiya/catalogservice/services/dtos/responses/campaignProductOfferResponses/CreatedCampaignProductOfferResponse.java
+++ catalogservice/src/main/java/com/etiya/catalogservice/services/dtos/responses/campaignProductOfferResponses/CreatedCampaignProductOfferResponse.java
@@ -1,4 +1,17 @@
 package com.etiya.catalogservice.services.dtos.responses.campaignProductOfferResponses;
 
+import com.etiya.catalogservice.entities.Campaign;
+import com.etiya.catalogservice.entities.ProductOffer;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class CreatedCampaignProductOfferResponse {
+    private String id;
+    private String campaignId;
+    private String productOfferId;
 }
--- catalogservice/src/main/java/com/etiya/catalogservice/services/dtos/responses/campaignProductOfferResponses/GetAllCampaignProductOfferResponse.java
+++ catalogservice/src/main/java/com/etiya/catalogservice/services/dtos/responses/campaignProductOfferResponses/GetAllCampaignProductOfferResponse.java
@@ -1,4 +1,14 @@
 package com.etiya.catalogservice.services.dtos.responses.campaignProductOfferResponses;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class GetAllCampaignProductOfferResponse {
+    private String id;
+    private String campaignId;
+    private String productOfferId;
 }
